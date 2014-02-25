0b554dc9caf5cfc21f50713bf793d2fd7ac18b33
atsanche <bossbiga123@gmail.com>
2014-02-25 08:09:11 -0700

    0.0.3.0
    Time GUI
    Basic Collision detection

---
+++ Assets/Chapter 3/Scripts/collisionDetection.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+@script RequireComponent(AudioSource)
+public var collection_beep : AudioClip;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+function OnControllerColliderHit(c:ControllerColliderHit){
+	print("collided with "+c.gameObject.tag);
+	if (c.gameObject.tag =="medpack") {
+		Destroy(c.gameObject);
+		audio.clip = collection_beep;
+		audio.Play();
+	}
+}
---
+++ Assets/Chapter 3/Scripts/timer.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+private var time: float;
+private var minutes: int;
+private var seconds:int;
+private var textToDisplay : String;
+private var guiTextToDisplayTime : GameObject;
+function Start () {
+	guiTextToDisplayTime = GameObject.Find("GUIText_timer");
+}
+
+function Update () {
+	time = time+Time.deltaTime;
+	minutes = time/60;
+	seconds = time%60;
+	textToDisplay = minutes+":"+seconds;
+	guiTextToDisplayTime.guiText.text = textToDisplay;
+}
